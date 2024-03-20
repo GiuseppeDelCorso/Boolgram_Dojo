bd37a225caac3f558b9b97e84b4d725b841d41df
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2024-03-20 17:14:00 +0100

    add shearch bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -30,7 +30,13 @@ export default {
                 </div>
                 <div class="col-4">
                     <nav class="navbar navbar-light bg-light">
-  
+                        <div class="container-fluid">
+                            <form class="d-flex">
+                                <input class="form-control me-2" type="search" placeholder="Search"
+                                    aria-label="Search">
+                            </form>
+                        </div>
+                    </nav>
                 </div>
                 <div class="col-4">
 
